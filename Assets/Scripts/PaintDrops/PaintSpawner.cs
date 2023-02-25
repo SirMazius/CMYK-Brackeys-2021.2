5aab817bfc415afe576891a28be5fcac00efead2
jpicogom <jmpicgom@yahoo.es>
2023-02-25 10:55:22 +0100

    Reorganizacion de carpetas scripts y jerarquia de escena

--- Assets/Scripts/PaintDrops/PaintSpawner.cs
+++ Assets/Scripts/PaintDrops/PaintSpawner.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static GameGlobals;
+using Sirenix.OdinInspector;
+
 
 public class PaintSpawner : MonoBehaviour
 {
